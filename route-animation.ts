4b7c29cab8e2e67334df8bde8fe66f631f298595
Aman Sultan Baig <41924807+AmanSultanBaig@users.noreply.github.com>
2019-10-28 22:53:49 +0500

    Create route-animation.ts

---
+++ route-animation.ts
@@ -0,0 +1,165 @@
+import {
+    transition,
+    trigger,
+    query,
+    style,
+    animate,
+    group,
+    animateChild
+} from '@angular/animations';
+export const slideInAnimation =
+    trigger('routeAnimations', [
+        transition('Login => *', [
+            query(':enter, :leave',
+                style({ position: 'fixed', width: '100%' }),
+                { optional: true }),
+            group([
+                query(':enter', [
+                    style({ transform: 'translateX(100%)' }),
+                    animate('0.5s ease-in-out',
+                        style({ transform: 'translateX(0%)' }))
+                ], { optional: true }),
+                query(':leave', [
+                    style({ transform: 'translateX(0%)' }),
+                    animate('0.5s ease-in-out',
+                        style({ transform: 'translateX(-100%)' }))
+                ], { optional: true }),
+            ])
+        ]),
+        transition('SignUp => *', [
+            query(':enter, :leave',
+                style({ position: 'fixed', width: '100%' }),
+                { optional: true }),
+            group([
+                query(':enter', [
+                    style({ transform: 'translateX(100%)' }),
+                    animate('0.5s ease-in-out',
+                        style({ transform: 'translateX(0%)' }))
+                ], { optional: true }),
+                query(':leave', [
+                    style({ transform: 'translateX(0%)' }),
+                    animate('0.5s ease-in-out',
+                        style({ transform: 'translateX(-100%)' }))
+                ], { optional: true }),
+            ])
+        ]),
+        transition('Forget => *', [
+            query(':enter, :leave',
+                style({ position: 'fixed', width: '100%' }),
+                { optional: true }),
+            group([
+                query(':enter', [
+                    style({ transform: 'translateX(100%)' }),
+                    animate('0.5s ease-in-out',
+                        style({ transform: 'translateX(0%)' }))
+                ], { optional: true }),
+                query(':leave', [
+                    style({ transform: 'translateX(0%)' }),
+                    animate('0.5s ease-in-out',
+                        style({ transform: 'translateX(-100%)' }))
+                ], { optional: true }),
+            ])
+        ]),
+        transition('Dashboard => *', [
+            query(':enter, :leave',
+                style({ position: 'fixed', width: '100%' }),
+                { optional: true }),
+            group([
+                query(':enter', [
+                    style({ transform: 'translateX(100%)' }),
+                    animate('0.5s ease-in-out',
+                        style({ transform: 'translateX(0%)' }))
+                ], { optional: true }),
+                query(':leave', [
+                    style({ transform: 'translateX(0%)' }),
+                    animate('0.5s ease-in-out',
+                        style({ transform: 'translateX(-100%)' }))
+                ], { optional: true }),
+            ])
+        ]),
+        transition('BuildCV => *', [
+            query(':enter, :leave',
+                style({ position: 'fixed', width: '100%' }),
+                { optional: true }),
+            group([
+                query(':enter', [
+                    style({ transform: 'translateX(100%)' }),
+                    animate('0.5s ease-in-out',
+                        style({ transform: 'translateX(0%)' }))
+                ], { optional: true }),
+                query(':leave', [
+                    style({ transform: 'translateX(0%)' }),
+                    animate('0.5s ease-in-out',
+                        style({ transform: 'translateX(-100%)' }))
+                ], { optional: true }),
+            ])
+        ]),
+        transition('Home => *', [
+            query(':enter, :leave',
+                style({ position: 'fixed', width: '100%' }),
+                { optional: true }),
+            group([
+                query(':enter', [
+                    style({ transform: 'translateX(100%)' }),
+                    animate('0.5s ease-in-out',
+                        style({ transform: 'translateX(0%)' }))
+                ], { optional: true }),
+                query(':leave', [
+                    style({ transform: 'translateX(0%)' }),
+                    animate('0.5s ease-in-out',
+                        style({ transform: 'translateX(-100%)' }))
+                ], { optional: true }),
+            ])
+        ]),
+        transition('Education => *', [
+            query(':enter, :leave',
+                style({ position: 'fixed', width: '100%' }),
+                { optional: true }),
+            group([
+                query(':enter', [
+                    style({ transform: 'translateX(100%)' }),
+                    animate('0.5s ease-in-out',
+                        style({ transform: 'translateX(0%)' }))
+                ], { optional: true }),
+                query(':leave', [
+                    style({ transform: 'translateX(0%)' }),
+                    animate('0.5s ease-in-out',
+                        style({ transform: 'translateX(-100%)' }))
+                ], { optional: true }),
+            ])
+        ]),
+        transition('Experience => *', [
+            query(':enter, :leave',
+                style({ position: 'fixed', width: '100%' }),
+                { optional: true }),
+            group([
+                query(':enter', [
+                    style({ transform: 'translateX(100%)' }),
+                    animate('0.5s ease-in-out',
+                        style({ transform: 'translateX(0%)' }))
+                ], { optional: true }),
+                query(':leave', [
+                    style({ transform: 'translateX(0%)' }),
+                    animate('0.5s ease-in-out',
+                        style({ transform: 'translateX(-100%)' }))
+                ], { optional: true }),
+            ])
+        ]),
+        transition('Skills => *', [
+            query(':enter, :leave',
+                style({ position: 'fixed', width: '100%' }),
+                { optional: true }),
+            group([
+                query(':enter', [
+                    style({ transform: 'translateX(100%)' }),
+                    animate('0.5s ease-in-out',
+                        style({ transform: 'translateX(0%)' }))
+                ], { optional: true }),
+                query(':leave', [
+                    style({ transform: 'translateX(0%)' }),
+                    animate('0.5s ease-in-out',
+                        style({ transform: 'translateX(-100%)' }))
+                ], { optional: true }),
+            ])
+        ]),
+    ]);
